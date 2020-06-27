ab3680a52e5284e4ce073ff1f6ad7c6a9b8b476a
antonrasegard <arasegard@gmail.com>
2020-06-27 07:32:59 +0000

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".box").on("click", function(){
+        let classNames = $(this).attr("class").split(" ");
+        let boxClass = classNames[0];
+        let className = classNames[1];
+        if ($(this).css("background-color") == "rgb(255, 0, 0)"){
+            $("." + className).css("background-color", "#000");
+        } else {
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "red");
+        }
+    });
+})
